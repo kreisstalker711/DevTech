b38ddee395bf3bf926e6196548240c70a761a35c
kreisdev@github <kathir20san@gmail.com>
2025-10-19 14:27:50 +0530

    page added and service page update

---
+++ script.js
@@ -0,0 +1,6 @@
+function movetopage1() {
+    window.location.href = "https://envirinosynthquiz.vercel.app/";
+}
+function movetopage2() {
+    window.location.href = "https://stemedtech.netlify.app/";
+}
